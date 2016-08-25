65b8b5f2a0fe1212b5c4f68746a4ded443b77774
Begaliev <azim.begaliev@gmail.cm>
2016-08-25 10:42:51 +0400

    урок 14

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -17,14 +17,5 @@ namespace Snake
                 pList.Add(p);
             }
         }
-
-        public override void Draw() // Наследник класс HorisontalLine переопределят метод Draw базового класса Figure
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-
-            base.Draw();
-
-            Console.ForegroundColor = ConsoleColor.White;
-        }
     }
 }
